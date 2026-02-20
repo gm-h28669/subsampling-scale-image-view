fe381b711d343dc70c5639bbbf7bc744f02473c5
Gianni Mellon <gianni.mellon@gmail.com>
2026-02-20 17:48:48 +0100

    Added some more images as resources for test purposes to sample app.

--- sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
+++ sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
@@ -2,13 +2,21 @@ package com.davemorrissey.labs.subscaleview.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.pages_activity.*
+import com.davemorrissey.labs.subscaleview.test.databinding.PagesActivityBinding
 
 class BasicFeaturesActivity : AppCompatActivity() {
+
+    private lateinit var binding: PagesActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pages_activity)
-        actionBar?.title = getString(R.string.basic_title)
-        imageView.setImage("/android_asset/sanmartino.jpg")
+
+        binding = PagesActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.basic_title)
+        // binding.imageView.orientation = 90
+        binding.imageView.setImage("/android_asset/Img0216.jpg")
+        // binding.imageView.setImage("/android_asset/P1040281.jpg")
+        // binding.imageView.setImage("/android_asset/P1050733.jpg")
     }
 }
