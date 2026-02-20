6c1b1374c86c655e72e952c1a1705fc3e103cc6d
Gianni Mellon <gianni.mellon@gmail.com>
2026-02-20 17:40:29 +0100

    Updated from Gradle to Kotlin Gradle build files.

---
+++ library/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    `maven-publish`
+}
+
+android {
+    compileSdk = 36
+    namespace = "com.davemorrissey.labs.subscaleview"
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("proguard-rules.txt")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.setSrcDirs(listOf("src/main/kotlin"))
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt")
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+        singleVariant("debug") {
+            withSourcesJar()
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "org.fossify"
+                artifactId = "subsampling-scale-image-view"
+                version = "3.2.0"
+            }
+            create<MavenPublication>("debug") {
+                from(components["debug"])
+                groupId = "org.fossify"
+                artifactId = "subsampling-scale-image-view-debug"
+                version = "3.2.0"
+            }
+        }
+        repositories {
+            maven {
+                name = "local"
+                url = uri(layout.buildDirectory.dir("localMaven"))
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.exifinterface)
+    implementation(libs.kotlin.stdlib)
+}
+
+tasks.named("build") {
+    finalizedBy("publishToMavenLocal")
+}
---
+++ sample/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    defaultConfig {
+        minSdk = 24
+        applicationId = "com.davemorrissey.labs.subscaleview.test"
+        versionCode = 4
+        versionName = "3.1.0"
+    }
+
+    sourceSets {
+        getByName("main") {
+            assets.setSrcDirs(listOf("assets"))
+            java.setSrcDirs(listOf("src/main/kotlin"))
+        }
+    }
+    namespace = "com.davemorrissey.labs.subscaleview.test"
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    implementation(project(":library"))
+    implementation(libs.kotlin.stdlib)
+    api(libs.androidx.appcompat)
+}
